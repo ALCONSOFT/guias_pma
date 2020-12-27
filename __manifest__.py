5cfee8a1d132f06d40f6d1628250dadf27d6ca6f
alconor1 <soporte1@alconsoft.net>
2020-12-26 21:22:29 -0500

    2020-12-26 - 21:00
    Avance del dia!

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.2020-12-21 - 14:00',
+    'version': '0.2020-12-24 - 22:30 ',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'stock', 'fincas_pma'],
